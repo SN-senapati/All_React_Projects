1914bdc4390823da28167b6fa4fe78ec073bd7fe
Sudipta <senapatisudipti@gmail.com>
2026-09-20 18:23:01 +0530

    changes some extra file

--- src/mini-project/Dice-Rolling-App/DiceRoller.jsx
+++ src/mini-project/Dice-Rolling-App/DiceRoller.jsx
@@ -9,6 +9,7 @@ export function DiceRoller(){
         setNumber(randomNumber)
         setNumber2(randomNumber2)
     }
+    console.log("hello chinu")
     const position = {
         1 : [[2,2]],
         2 : [[1,1], [3,3]],
